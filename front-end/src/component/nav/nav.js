1b2bf040ff3fcbcc304ea809262b302b18ad0034
gator <lunruh946@gmail.com>
2025-07-27 16:27:02 -0600

    nav buttons
    images added

--- front-end/src/component/nav/nav.js
+++ front-end/src/component/nav/nav.js
@@ -4,19 +4,43 @@ import "./nav.css";
 const Nav = () => {
   return (
     <div className="nav-box">
-      <h2 className="nav-text">NAVIGATION</h2>
+      <h2>NAVIGATION</h2>
       <ul>
         <li>
           <a href="/">
-            <img src="./images/buttons/home_navBtn.png" alt="home" />
+            <img src="./images/buttons/homeNav.png" alt="home" />
           </a>
         </li>
-        <li><a href="/about">About GATOR</a></li>
-        <li><a href="/friends">Friends</a></li>
-        <li><a href="/byf_dni">Interactions</a></li>
-        <li><a href="/guestbook">Guestbook</a></li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="/sitemap">Sitemap</a></li>
+        <li>
+          <a href="/about">
+            <img src="./images/buttons/aboutNav.png" alt="about" />
+          </a>
+        </li>
+        <li>
+          <a href="/friends">
+            <img src="./images/buttons/friendsNav.png" alt="friends" />
+          </a>
+        </li>
+        <li>
+          <a href="/byf_dni">
+            <img src="./images/buttons/interactNav.png" alt="byf_dni" />
+          </a>
+        </li>
+        <li>
+          <a href="/guestbook">
+            <img src="./images/buttons/guestbookNav.png" alt="guestbook" />
+          </a>
+        </li>
+        <li>
+          <a href="/projects">
+            <img src="./images/buttons/projectNav.png" alt="projects" />
+          </a>
+        </li>
+        <li>
+          <a href="/sitemap">
+            <img src="./images/buttons/sitemapNav.png" alt="sitemap" />
+          </a>
+        </li>
       </ul>
     </div>
   );
